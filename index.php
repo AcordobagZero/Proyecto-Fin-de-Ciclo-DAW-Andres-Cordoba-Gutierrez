4f833b2fd2f4dfd6fff6ed46cc305f39f47dc3ab
AcordobagZero <79990067+AcordobagZero@users.noreply.github.com>
2022-06-10 13:56:09 +0200

    Add files via upload
    Implementación de modulo de previsión del tiempo

--- index.php
+++ index.php
@@ -1,39 +1,33 @@
-<! DOCTYPE HTML PUBLIC "- // W3C // DTD HTML 4.01 Transitional // EN" "http://www.w3.org/TR/html4/loose.dtd">
- <html>
-   <head>
-     <title>Titulo</title>
-     <h2>Titulo</h2>
-     <Meta http-equiv = "refresh" content = "3600; url = index.php">
-     <Meta http-equiv = "Content-Type" content = "text / html; charset = UTF-8" />
-     <Meta http-equiv = contenido "Content-Language" = "es" />
-     <Meta name = "autor" lang = "es" content = "subir" />
-     <Meta name = contenido de "derechos de autor" = "subir ©" />
-     <Meta http-equiv = "Cache-Control" content = "no-cache">
-     <Meta http-equiv = "Pragma" content = "no-cache">
-     <Meta http-equiv = "Expires" content = "0">
-     <Meta http-equiv = "refresh" content = "3600; url = index.php">
-     <Script type = "text / javascript" src = "// ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-     <Script type = "text / javascript" src = "javascript.js"></script>
-     <link rel = "stylesheet" type = "text / css" href = "style.css"/>
-   </head>
-   <body>
+.<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <title>Interfaz RaspBerry</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    <meta http-equiv="Content-Language" content="Fr"/>
+    <meta name="Author" lang="fr" content="Torna"/>
+    <meta name="Copyright" content="© Torna"/>
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="refresh" content="3600;url=index.php">
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="javascript.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+  <body>
 
-     <div id = "principal"><? Php // fecha de eco ( "Ymd H: i: s') - <div id =" semental ">!?  ?> </div ->
-       <div id = "reloj"></div>
-       <div id = "Tiempo"></div>
-       <div id = "meteo_black"></div>
-       <div id = "TS3"> </div>
-       <div id = "ping"> </div>
-       <div id = "análisis"></div>
-       <div id = "VPN"></div>
-       <div id = "latencia"> </div>
-       <div id = "tierra"> <img id = "img_earth" src = "PICT / blank.png"></div>
-       <div id = "luna"> <img id = "img_moon" src = "PICT / blank.png"></div>
-       <div id = "ifstat_oberon_up"> <img id = "img_oberon_up" src = "PICT / blank.png"></div>
-       <div id = "ifstat_oberon_down"> <img id = "img_oberon_down" src = "PICT / blank.png"></div>
-     </div>
+    <div id="main">
+      <div id="horloge"></div>
+      <div id="meteo"></div>
+      <div id="ts3"></div>
+      <div id="ping"></div>
+      <div id="vpn"></div>
+      <div id="latency"></div>
+      <div id="earth"><img id="img_earth" src="pict/blank.png"></div>
+      <div id="moon"><img id="img_moon" src="pict/blank.png"></div>
+      <div id="ifstat_oberon_up"><img id="img_oberon_up" src="pict/blank.png"></div>
+      <div id="ifstat_oberon_down"><img id="img_oberon_down" src="pict/blank.png"></div>
+    </div>
 
-     <? Php include ( '.inc.php');  analytics de eco ();  ?>
-
-   </body>
- </html>
+  </body>
+</html>
--- javascript.js
+++ javascript.js
@@ -1,56 +1,154 @@
-/* Inicializar las funciones de todos los modulos a cargar la pagina */
-$(Document) ready(function () {
-  fonction_nom_du_module_1();
-  fonction_nom_du_module_2();
-
+$(document).ready(function() {
+   horloge();
+   ts3();
+   xplanet();
+   ifstat();
+   meteo();
+   vpn();
+   ping();
 });
 
- /* Inicializar las variables de tiempo de espera para cada modulo */
- //timeout_nom_du_module_1 var ; 
- //timeout_nom_du_module_2 var ;
+/* meteo */
 
+var meteo_timeout;
 
-/* Definicion de la funcion para la nom_du_module_1 modulo */
-
-function nom_du_module_1() {
+function meteo ()
+{
   $.ajax({
-    async: falso,
+    async : false,
     type: "GET",
     url: "./ajax.php",
-    data: "bloque = nom_du_module_1",
-    success: function (html) {
-      $("# Nom_du_module_1"), HTML(HTML);
+    data: "block=meteo",
+    success: function(html){
+      $("#meteo").html(html);
     }
   });
 
-  g = setTimeout("nom_du_module_1 ()", 10000);
+  meteo_timeout = setTimeout("meteo()", 3600000);
 }
 
+/* horloge */
 
-function horloge() {
-  dows = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-  meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var horloge_timeout;
 
-  now = new Date;
-  heure = now.getHours();
-  min = now.getMinutes();
-  sec = now.getSeconds();
+function horloge()
+{
+  dows  = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  mois  = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+  now          = new Date;
+  heure        = now.getHours();
+  min          = now.getMinutes();
+  sec          = now.getSeconds();
   jour_semaine = dows[now.getDay()];
-  jour = now.getDate();
-  mois = mois[now.getMonth()];
-  annee = now.getFullYear();
+  jour         = now.getDate();
+  mois         = mois[now.getMonth()];
+  annee        = now.getFullYear();
 
-  if (sec < 10) { sec0 = "0"; } else { sec0 = ""; }
-  if (min < 10) { min0 = "0"; } else { min0 = ""; }
-  if (heure < 10) { heure0 = "0"; } else { heure0 = ""; }
+  if (sec < 10){sec0 = "0";}else{sec0 = "";}
+  if (min < 10){min0 = "0";}else{min0 = "";}
+  if (heure < 10){heure0 = "0";}else{heure0 = "";}
 
-  horloge_heure = heure + ":" + min0 + min;
-  horloge_date = "<span class='horloge_grey'>" + jour_semaine + "</span> " + jour + " " + mois + " <span class='horloge_grey'>" + annee + "</span>";
+  horloge_heure   = heure + ":" + min0 + min;
+  horloge_date    = "<span class='horloge_grey'>" + jour_semaine + "</span> " + jour + " " + mois + " <span class='horloge_grey'>" + annee + "</span>";
   horloge_content = "<div class='horloge_heure'>" + horloge_heure + "</div><div class='horloge_date'>" + horloge_date + "</div>";
 
   $("#horloge").html(horloge_content);
 
   horloge_timeout = setTimeout("horloge()", 1000);
+}
+
+/* TS3 */
+
+var ts3_timeout;
+
+function ts3 ()
+{
+  $.ajax({
+    async : false,
+    type: "GET",
+    url: "./ajax.php",
+    data: "block=ts3",
+    success: function(html){
+      $("#ts3").html(html);
+    }
+  });
+
+  ts3_timeout = setTimeout("ts3()", 10000);
+}
+
+/* PING */
+
+var ping_timeout;
+
+function ping ()
+{
+  $.ajax({
+    async : false,
+    type: "GET",
+    url: "./ajax.php",
+    data: "block=ping",
+    success: function(html){
+      $("#ping").html(html);
+    }
+  });
+
+  ping_timeout = setTimeout("ping()", 30000);
+}
+
+/* VPN */
+
+var vpn_timeout;
+
+function vpn ()
+{
+  $.ajax({
+    async : false,
+    type: "GET",
+    url: "./ajax.php",
+    data: "block=vpn",
+    success: function(html){
+      $("#vpn").html(html);
+    }
+  });
+
+  vpn_timeout = setTimeout("vpn()", 5000);
+}
+
+/* xplanet */
+
+var xplanet_timeout;
+
+function xplanet () {
+
+  var now       = new Date().getTime();
+  var img_earth = $("<img />").attr("src", "xplanet/img/xplanet_earth.png?"+now);
+  var img_mon   = $("<img />").attr("src", "xplanet/img/xplanet_moon.png?"+now);
+
+  $("#img_earth").attr("src", "xplanet/img/xplanet_earth.png?"+now);
+  $("#img_moon").attr("src", "xplanet/img/xplanet_moon.png?"+now);
+
+  xplanet_timeout = setTimeout("xplanet()", 600000);
+}
+
+/* ifstat */
+
+var ifstat_timeout;
+
+function ifstat () {
+
+  var now             = new Date().getTime();
+
+  var url_eth1_down = "ajax.php?block=ifstat&eth=wan&up_down=down&max=2000&hour="+now;
+  var url_eth1_up   = "ajax.php?block=ifstat&eth=wan&up_down=up&max=150&hour="+now;
+
+  var img_oberon_down = $("<img />").attr("src", url_eth1_down);
+  $("#img_oberon_down").attr("src", url_eth1_down);
+
+  var img_oberon_up = $("<img />").attr("src", url_eth1_up);
+  $("#img_oberon_up").attr("src", url_eth1_up);
+
+  ifstat_timeout = setTimeout("ifstat()", 5000);
 }
 
 
