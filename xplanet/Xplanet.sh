10b9bf663ef6ed87a5c7e16bf9a6c769a251ded2
AcordobagZero <79990067+AcordobagZero@users.noreply.github.com>
2022-06-13 15:40:37 +0200

    Add files via upload
    Modulo de vista terrestre y lunar

---
+++ xplanet/Xplanet.sh
@@ -0,0 +1,7 @@
+#! / Bin / sh
+
+ #TIERRA
+ xplanet -conf /var/www/html/xplanet/xplanet.conf -output /var/www/html/xplanet/img/xplanet_earth.png -wait 120 -body earth -geometry 500x500 -latitude 30 -longitude 0    
+
+ #LUNA
+ xplanet -conf /var/www/html/xplanet/xplanet.conf -output /var/www/html/xplanet/img/xplanet_moon.png -wait 600 -body moon -geometry 250x250 
--- xplanet/xplanet_clouds.sh
+++ xplanet/xplanet_clouds.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-pat=/home/jarvis/xplanet/img/
-tmp=$pat"tmp_clouds_2048.jpg"
-img=$pat"clouds_2048.jpg"
+$pat=/var/www/html/xplanet/img  # destino de la imagen descargada
+$tmp=$pat"tmp_clouds_2048.jpg"  # "tmp_clouds_2048.jpg"
+$img=$pat"clouds_2048.jpg"  # nombre del archivo final
 
-rm $tmp
+rm $tmp   # borrar el archivo temporal de edad
 
 wget -O $tmp http://xplanet.sourceforge.net/clouds/clouds_2048.jpg
 
-if [ -f $tmp ] ; then
-  mogrify -resize 2000x1000 $tmp
+if [ -f $tmp ] ; then   # si el archivo se ha descargado ...
+  mogrify -resize 2000x1000 $tmp  # redimenssionne la imagen descargada de manera que sea la misma resolucion que el mapa "dia"
   if [ -f $img ] ; then
     rm $img
   fi
-  mv $tmp $img
-  chown -R jarvis:www-data $pat && chmod -R 775 $pat
+  mv $tmp $img  # sustituye a la antigua imagen con el nuevo
+  chown -R jarvis:www-data $pat && chmod -R 775 $pat  # cambia los permisos en el archivo
 fi
+
